0c0f8b7824c041d1ddc1bf9ba963819c6cc7d425
Matt Howe <matt@matthowe.org>
2025-08-07 10:55:33 -0400

    Re-enabled and tailored tray

--- install/desktop/set-gnome-extensions.sh
+++ install/desktop/set-gnome-extensions.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-#Re-enable the dock
+# Re-enable the dock
 gnome-extensions enable ubuntu-dock@ubuntu.com
 
 # Do not show unmounted volumes in the dock
@@ -9,8 +9,19 @@ gsettings set org.gnome.shell.extensions.dash-to-dock show-unmounted-volumes fal
 # Disable panel mode
 gsettings set org.gnome.shell.extensions.dash-to-dock extend-height false
 
-#Set dock icon size
+# Set dock icon size
 gsettings set org.gnome.shell.extensions.dash-to-dock dash-max-icon-size 32
+
+# Set workspaces to 4
+gsettings set org.gnome.desktop.wm.preferences num-workspaces 4
+
+# Enable the panel appindicators
+gnome-extensions enable ubuntu-appindicators@ubuntu.com
+
+# Disable the Ulauncher panel icon
+sed -i 's/"show-indicator-icon": true,/"show-indicator-icon": false,/' ~/.config/ulauncher/settings.json
+pkill ulauncher
+nohup ulauncher > /dev/null 2>&1 &
 
 # Configure Tactile 
 gsettings set org.gnome.shell.extensions.tactile col-0 1
